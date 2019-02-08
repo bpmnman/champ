f36d1cfb693637a13d34c0c92ef3f4032bedf984
Dorjee Tsering <dorjee.tsering@paytm.com>
2019-02-08 12:00:27 -0500

    first

---
+++ src/main/java/com/dorjee/champ/ChampApplication.java
@@ -0,0 +1,14 @@
+package com.dorjee.champ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChampApplication.class, args);
+	}
+
+}
+

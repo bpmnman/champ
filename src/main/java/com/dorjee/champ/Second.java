bcad1e5d71388342e6dbf1804ea5884ca78b4378
Dorjee Tsering <dorjee.tsering@paytm.com>
2019-02-12 23:53:20 -0500

    removed method

--- src/main/java/com/dorjee/champ/Second.java
+++ src/main/java/com/dorjee/champ/Second.java
@@ -3,7 +3,6 @@ package com.dorjee.champ;
 public interface Second {
      String getName();
      String getAddress();
-     int getNumber();
      String getCard();
      int getLottery();
      String getRoad();

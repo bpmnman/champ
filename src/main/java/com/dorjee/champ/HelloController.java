3760d3ff59212de15507d09fb7d019c789d70df4
Dorjee Tsering <dorjee.tsering@paytm.com>
2019-02-13 00:12:51 -0500

    changed message

--- src/main/java/com/dorjee/champ/HelloController.java
+++ src/main/java/com/dorjee/champ/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("/")
     public String index() {
-        return "Greetings flasdfrom Spring Boot!";
+        return "Greetings rom Spring Boot!";
     }
 }
